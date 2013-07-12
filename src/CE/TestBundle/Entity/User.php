eba6daff02f94718a7f856d90b68a7e9be11224a
mrkarimi <m.r.karimi.j@gmail.com>
2013-07-12 23:26:11 +0430

    Added FOSUserBundle and configured it (corrected last bugs)

--- src/CE/TestBundle/Entity/User.php
+++ src/CE/TestBundle/Entity/User.php
@@ -20,14 +20,7 @@ class User extends BaseUser
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="username", type="string", length=255)
-     */
-    private $username;
+    protected $id;
 
     /**
      * @var
@@ -65,29 +58,6 @@ class User extends BaseUser
     public function getId()
     {
         return $this->id;
-    }
-
-    /**
-     * Set username
-     *
-     * @param string $username
-     * @return User
-     */
-    public function setUsername($username)
-    {
-        $this->username = $username;
-    
-        return $this;
-    }
-
-    /**
-     * Get username
-     *
-     * @return string 
-     */
-    public function getUsername()
-    {
-        return $this->username;
     }
 
     /**
